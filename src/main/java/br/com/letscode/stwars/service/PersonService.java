d3e17bf2031b078c53bc2f6bfae8199ff28ba1e5
Vitória Toebe <vitoriatoebe@gmail.com>
2022-03-07 23:17:32 -0300

    refactor: separate services
    removed locale, base and report handlers

--- src/main/java/br/com/letscode/stwars/service/PersonService.java
+++ src/main/java/br/com/letscode/stwars/service/PersonService.java
@@ -20,9 +20,8 @@ import java.util.Optional;
 public class PersonService {
 
     private final PersonRepository personRepository;
-    private final RebellionRepository rebellionRepository;
-    private final BaseRepository baseRepository;
     private final PersonMapper personMapper;
+    private final LocaleService localeService;
 
     public void testRepository(){
         Optional<PersonEntity> personEntity = personRepository.findById(5L);
@@ -36,60 +35,14 @@ public class PersonService {
         InventoryEntity inventory = EntityUtils.getInventory(person);
         inventory.setPerson(person);
 
-        LocaleEntity locale = EntityUtils.getLocale(person);
-        locale.setPerson(person);
-
-        locale.setBase(this.getBase(request.getBase()));
-
+        localeService.saveLocale(request, person);
         personRepository.save(person);
-    }
-
-    // todo
-    private BaseEntity getBase(String name) {
-        return baseRepository.findById(name)
-                .get(); // remover
-                // Todo criar Exception
-                //.orElseThrow( () -> new());
     }
 
     public void updateLocale(LocaleRequestDto request, Long personId) {
         PersonEntity person = personRepository.findById(personId).get(); //todo -> orElseThrow( EntityNotFound )
 
-        LocaleEntity locale = EntityUtils.getLocale(person);
-
-        locale.setLatitude(request.getLatitude());
-        locale.setLongitude(request.getLongitude());
-        locale.setBase(this.getBase(request.getBase()));
-
+        localeService.updateLocale(request, person);
         personRepository.save(person);
     }
-
-    public void reportPerson(ReportRequestDto request, Long personId) {
-        //todo validar se os ids são iguais
-        RebellionEntity rebellion = new RebellionEntity();
-
-        RebellionId rebellionId = new RebellionId();
-        //todo -> orElseThrow( EntityNotFound )
-        rebellionId.setReportByPerson(personRepository.findById(personId).get());
-
-        PersonEntity personReport = personRepository.findById(request.getReportPersonId()).get();
-        rebellionId.setPerson(personReport);
-
-        rebellion.setId(rebellionId);
-        rebellion.setDescription(request.getDescription());
-
-        rebellionRepository.save(rebellion);
-
-        // todo - Marcação do status de traição no person.
-        // todo tranformar em static o número 3
-        if (rebellionRepository.countReport(personReport.getId()) > 3 ) {
-            personReport.setFaction(FactionEnum.EMPIRE);
-            personRepository.save(personReport);
-        };
-
-
-    }
-
-
-
 }
