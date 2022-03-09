66580e1d9a0793dfc5f2bde237d620a3b9736b61
Vitória Toebe <vitoriatoebe@gmail.com>
2022-03-09 03:52:45 -0300

    feat: add trade end point
    • refactor: add meaningful end points

--- src/main/java/br/com/letscode/stwars/controller/MarketPlaceController.java
+++ src/main/java/br/com/letscode/stwars/controller/MarketPlaceController.java
@@ -1,7 +1,9 @@
 package br.com.letscode.stwars.controller;
 
 import br.com.letscode.stwars.dto.MarketPlaceDto;
+import br.com.letscode.stwars.dto.PersonIdDto;
 import br.com.letscode.stwars.model.MarketPlaceEntity;
+import br.com.letscode.stwars.model.PersonEntity;
 import br.com.letscode.stwars.service.MarketPlaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +17,19 @@ public class MarketPlaceController {
 
     private final MarketPlaceService service;
 
-    @PostMapping
+    @PostMapping("/create-offer")
     public void insertOffer(@RequestBody MarketPlaceDto requestDto) {
         service.insertNewOffer(requestDto);
     }
 
     //todo by filters
-    @GetMapping()
+    @GetMapping("/offers")
     public List<MarketPlaceEntity> getListByMarketPlace() {
         return service.getListByMarketPlace();
     }
 
+    @PatchMapping("/offers/{offerId}/trade")
+    public void tradeItems(@RequestBody PersonIdDto personId, @PathVariable("offerId") Long offerId){
+        service.tradeItems(personId, offerId);
+    }
 }
