dc4b3699dec6916de39d9805242ff62cb063ae26
Vitória Toebe <vitoriatoebe@gmail.com>
2022-03-10 01:38:32 -0300

    refactor: implements Serializable

--- src/main/java/br/com/letscode/stwars/model/PersonEntity.java
+++ src/main/java/br/com/letscode/stwars/model/PersonEntity.java
@@ -3,17 +3,20 @@ package br.com.letscode.stwars.model;
 import br.com.letscode.stwars.enums.FactionEnum;
 import br.com.letscode.stwars.enums.GenreEnum;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "person")
-public class PersonEntity {
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class PersonEntity implements Serializable {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
     private String name;
